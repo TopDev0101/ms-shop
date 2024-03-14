b46c30be789fcb022d0b8a3388d36a139ea06aa0
TopDev0101 <topdev0101@gmail.com>
2024-03-13 22:30:50 -0700

    added login page

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -10,7 +10,7 @@ class Education extends Component {
   }
 
   render() {
-    var b,a,c;
+    var b,a,c,d;
     const education = this.props.education.map(edu => (
       <tr key={edu._id}>
         <td>{edu.school}</td>
